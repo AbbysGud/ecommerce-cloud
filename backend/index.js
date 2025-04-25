0429b4e877a89cd0001f44b7704890d8010a6c30
Ariq Bagus Sugiharto <ariqbagus19@gmail.com>
2025-04-26 01:18:46 +0700

    test3

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Database connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
